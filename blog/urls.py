60d04328496f055dd2a429753f55c8ea2bb9a209
vivekhas3 <vivekhas3@gmail.com>
2017-10-13 19:25:48 +0530

    renamed import to absolute

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 
-from views import show_article
+from project.blog.views import show_article
 urlpatterns = patterns('',
     # Examples:
     url(r'^(?P<path>.*)/?$', show_article, name='show_article'),
